e13940f99cf86a485498ea4d60fb1a946cca7a4a
Pep Mascaro Monserrat <pep.mascaro.monserrat@ba.com>
2022-11-01 22:13:25 +0000

    Creating dataframe

--- useful_functions.py
+++ useful_functions.py
@@ -1,1 +1,30 @@
-# hello world
+#-----------
+#
+# Import required libraries
+# 
+#-----------
+import numpy as np
+import pandas as pd
+
+# to generate random numbers
+import random
+
+#-----------
+#
+# Create empty dataframe from scratch and storing it in variable df
+#
+#-----------
+df = pd.DataFrame()
+
+# create a list for each column
+name = ['Toni','Joanmi','Biel','Pere','Xisco','Xavi','Miquel','Jordi','Toni']
+surname = ['Rigo','Perello','Nadal','Manresa','Adrover','Coves','Caldentey','Bauza','Mestre']
+hobby = ['Running', 'Triathlon','Crossfit', 'Bouldering', 'Yoga', 'Running', 'Crossfit', 'Sailing']
+city_residence = ['Felanitx', 'Palma', 'Barcelona', 'Barcelona', 'Portocolom', 'Eivissa', 'Felanitx', 'Felanitx']
+country_residence = ['Spain', 'Spain', 'Spain', 'Spain', 'Spain', 'Spain', 'Spain', 'Spain']
+no_cousins = [13, 13, 11, 12, 12, 14, 15, 11, 4]
+
+# list of column names
+columns=['name','surname',hobby,city_residence,country_residence,no_cousins]
+
+df = pd.DataFrame( ,columns = columns)
